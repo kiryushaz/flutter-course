0f8b2336207c25aa48121d98cc1b2acdc9b95f86
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-02-29 19:30:01 +0600

    вынес класс App в отдельный файл

---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CoffeeShopApp extends StatelessWidget {
+  const CoffeeShopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
