2ef9ec1e55af7bf3551d7be7797237c05f7e46fb
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-03-14 14:06:14 +0600

    карточки отображаются на экране

--- lib/src/features/menu/view/menu_screen.dart
+++ lib/src/features/menu/view/menu_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course/src/features/menu/view/widgets/category_button.dart';
+import 'package:flutter_course/src/features/menu/view/widgets/coffeecard.dart';
 import '../data/category_data.dart';
+import '../data/coffee_data.dart';
+import '../model/coffee.dart';
 
 class MenuScreen extends StatefulWidget {
   const MenuScreen({super.key});
@@ -24,31 +27,59 @@ class _MenuScreenState extends State<MenuScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-          child: SizedBox(
-            height: 68.0,
-            child: ListView.separated(
-              padding: const EdgeInsets.all(16.0),
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              itemBuilder: (context, index) => CategoryButton(
-                  text: categories[index].name,
-                  isActive: index == _selectedIndex,
-                  onPressed: () => selectCategory(index)),
-              separatorBuilder: (context, index) => const SizedBox(width: 8.0),
-            ),
-          ),
-        ),
-        // Expanded(
-        //   child: ListView.builder(
-        //     itemCount: categories.length,
-        //     itemBuilder: (context, index) {
-        //       return Text(categories[index].name, style: Theme.of(context).textTheme.titleLarge);
-        //     },
-        //   )
-        // )
-      ],
+      slivers: <Widget>[
+            SliverAppBar(
+              pinned: true,
+              elevation: 0,
+              flexibleSpace: SizedBox(
+                height: 36,
+                child: ListView.separated(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) => CategoryButton(
+                      text: categories[index].name,
+                      isActive: index == _selectedIndex,
+                      onPressed: () => selectCategory(index)),
+                  separatorBuilder: (context, index) =>
+                      const SizedBox(width: 8.0),
+                ),
+              ),
+            )
+          ] +
+          categories
+              .map((category) {
+                List<Coffee> coffeeCat = coffee
+                    .where((element) => element.category == category)
+                    .toList();
+                return [
+                  SliverPadding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    sliver: SliverToBoxAdapter(
+                      child: Text(category.name,
+                          style: Theme.of(context).textTheme.titleLarge),
+                    ),
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.all(16.0),
+                    sliver: SliverGrid(
+                        gridDelegate:
+                            const SliverGridDelegateWithMaxCrossAxisExtent(
+                          maxCrossAxisExtent: 250.0,
+                          mainAxisExtent: 196,
+                          mainAxisSpacing: 10.0,
+                          crossAxisSpacing: 10.0,
+                          childAspectRatio: 1.0,
+                        ),
+                        delegate: SliverChildBuilderDelegate((context, index) {
+                          return Coffeecard(coffee: coffeeCat[index]);
+                        }, childCount: coffeeCat.length)
+                    ),
+                  ),
+                ];
+              })
+              .expand((element) => element)
+              .toList(),
     ));
   }
 }
