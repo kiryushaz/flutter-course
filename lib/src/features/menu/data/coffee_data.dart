ed406de77461e9c6dc47d102def3bf7719fa600e
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-03-14 20:08:42 +0600

    изменен набор данных

--- lib/src/features/menu/data/coffee_data.dart
+++ lib/src/features/menu/data/coffee_data.dart
@@ -2,14 +2,14 @@ import 'package:flutter_course/src/features/menu/data/category_data.dart';
 import 'package:flutter_course/src/features/menu/model/coffee.dart';
 
 List<Coffee> coffee = [
-  Coffee(name: "Олеато", price: 139, image: "images/coffee.png", category: categories[0]),
-  Coffee(name: "Латте", price: 139, category: categories[0]),
-    Coffee(name: "Олеато", price: 139, image: "images/coffee.png", category: categories[0]),
-  Coffee(name: "Латте", price: 139, category: categories[0]),
-  Coffee(name: "Олеато2", price: 139, image: "images/coffee.png", category: categories[1]),
-  Coffee(name: "Латте2", price: 139, category: categories[1]),
-  Coffee(name: "Олеато3", price: 139, image: "images/coffee.png", category: categories[2]),
-  Coffee(name: "Латте3", price: 139, category: categories[2]),
-  Coffee(name: "Олеато4", price: 139, image: "images/coffee.png", category: categories[3]),
-  Coffee(name: "Латте4", price: 139, category: categories[3]),
+  Coffee(name: "Эспрессо", price: 139, image: "assets/images/coffee.png", category: categories[0]),
+  Coffee(name: "Американо", price: 139, category: categories[0]),
+  Coffee(name: "Ристретто", price: 139, image: "assets/images/coffee.png", category: categories[0]),
+  Coffee(name: "Романо", price: 139, category: categories[0]),
+  Coffee(name: "Капучино", price: 139, image: "assets/images/coffee.png", category: categories[1]),
+  Coffee(name: "Латте", price: 139, category: categories[1]),
+  Coffee(name: "Чай черный", price: 139, image: "assets/images/coffee.png", category: categories[2]),
+  Coffee(name: "Чай зеленый", price: 139, category: categories[2]),
+  Coffee(name: "Раф Лимонный пирог", price: 139, image: "assets/images/coffee.png", category: categories[3]),
+  Coffee(name: "Раф Ванильный", price: 139, category: categories[3]),
 ];
