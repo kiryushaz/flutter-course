6ea287398f9df7522c1f460055d12b55a7049321
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-03-21 14:12:06 +0600

    fixes

--- lib/src/features/menu/view/menu_screen.dart
+++ lib/src/features/menu/view/menu_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_course/src/features/menu/view/widgets/category_button.dart';
 import 'package:flutter_course/src/features/menu/view/widgets/coffeecard.dart';
 import '../data/category_data.dart';
@@ -19,14 +20,14 @@ class _MenuScreenState extends State<MenuScreen> {
   @override
   void initState() {
     _scrollController.addListener(() {
-      print(_scrollController.offset);
+      // print(_scrollController.offset);
     });
     super.initState();
   }
 
   void scrollTo(index) {
     setState(() {
-      final targetContext = categories[index].key.currentContext;
+      final targetContext = categories[index]['key'].currentContext;
       if (targetContext != null) {
         Scrollable.ensureVisible(targetContext,
             duration: const Duration(milliseconds: 500), curve: Curves.ease);
@@ -45,67 +46,68 @@ class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(
-      controller: _scrollController,
-      slivers: <Widget>[
-            SliverAppBar(
-              pinned: true,
-              elevation: 0,
-              shadowColor: Colors.transparent,
-              surfaceTintColor: Colors.transparent,
-              flexibleSpace: SizedBox(
-                height: 52,
-                child: ListView.separated(
-                  shrinkWrap: true,
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 16.0),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categories.length,
-                  itemBuilder: (context, index) => CategoryButton(
-                      text: categories[index].name,
-                      isActive: index == _selectedIndex,
-                      onPressed: () {
-                        selectCategory(index);
-                        scrollTo(index);
-                      }),
-                  separatorBuilder: (context, index) =>
-                      const SizedBox(width: 8.0),
+        body: CustomScrollView(controller: _scrollController, slivers: <Widget>[
+      SliverAppBar(
+        pinned: true,
+        elevation: 0,
+        shadowColor: Colors.transparent,
+        surfaceTintColor: Colors.transparent,
+        flexibleSpace: SizedBox(
+          child: ListView.separated(
+            shrinkWrap: true,
+            padding:
+                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            scrollDirection: Axis.horizontal,
+            itemCount: categories.length,
+            itemBuilder: (context, index) {
+              final category = categories[index];
+              return CategoryButton(
+                text: category['data'].name,
+                isActive: index == _selectedIndex,
+                onPressed: () {
+                  selectCategory(index);
+                  scrollTo(index);
+                });
+            },
+            separatorBuilder: (context, index) => const SizedBox(width: 8.0),
+          ),
+        ),
+      ),
+      SliverToBoxAdapter(
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: categories.length,
+          itemBuilder: (context, index) {
+            final category = categories[index];
+            List<Coffee> coffeeCat =
+                coffee.where((t) => t.category == category['data']).toList();
+            return CustomScrollView(shrinkWrap: true, slivers: [
+              SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                sliver: SliverToBoxAdapter(
+                  key: category['key'],
+                  child: Text(category['data'].name,
+                      style: Theme.of(context).textTheme.titleLarge),
                 ),
               ),
-            )
-          ] +
-          categories
-              .map((category) {
-                List<Coffee> coffeeCat = coffee
-                    .where((element) => element.category == category)
-                    .toList();
-                return [
-                  SliverPadding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    sliver: SliverToBoxAdapter(
-                      key: category.key,
-                      child: Text(category.name,
-                          style: Theme.of(context).textTheme.titleLarge),
+              SliverPadding(
+                padding: const EdgeInsets.all(16.0),
+                sliver: SliverGrid(
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 320,
+                      mainAxisExtent: 216,
+                      mainAxisSpacing: 16.0,
+                      crossAxisSpacing: 16.0,
                     ),
-                  ),
-                  SliverPadding(
-                    padding: const EdgeInsets.all(16.0),
-                    sliver: SliverGrid(
-                        gridDelegate:
-                            const SliverGridDelegateWithMaxCrossAxisExtent(
-                          maxCrossAxisExtent: 320,
-                          mainAxisExtent: 216,
-                          mainAxisSpacing: 16.0,
-                          crossAxisSpacing: 16.0,
-                        ),
-                        delegate: SliverChildBuilderDelegate((context, index) {
-                          return Coffeecard(coffee: coffeeCat[index]);
-                        }, childCount: coffeeCat.length)),
-                  ),
-                ];
-              })
-              .expand((element) => element)
-              .toList(),
-    ));
+                    delegate: SliverChildBuilderDelegate((context, index) {
+                      return Coffeecard(coffee: coffeeCat[index]);
+                    }, childCount: coffeeCat.length)),
+              ),
+            ]);
+          },
+        ),
+      )
+    ]));
   }
 }
