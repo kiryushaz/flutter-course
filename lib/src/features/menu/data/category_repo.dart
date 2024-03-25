db717f8cf43c80f8c72812ca6b23ca1353ac7860
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-03-25 17:07:43 +0600

    replace func to debugPrint

--- lib/src/features/menu/data/category_repo.dart
+++ lib/src/features/menu/data/category_repo.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_course/src/features/menu/model/category.dart';
 
 final dio = Dio(BaseOptions(baseUrl: 'https://coffeeshop.academy.effective.band/api/v1'));
@@ -9,7 +10,7 @@ Future<List<Category>> fetchCategories({int page = 0, int limit = 25}) async {
   if (response.statusCode == 200) {
     final data = response.data!['data'].map((json) => Category.fromJson(json));
     final result = List<Category>.from(data);
-    print('fetching categories');
+    debugPrint('fetching categories');
     return result;
   } else {
     throw Exception('Failed to load categories');
--- lib/src/features/menu/data/product_repo.dart
+++ lib/src/features/menu/data/product_repo.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_course/src/features/menu/model/product.dart';
 
 final dio = Dio(BaseOptions(baseUrl: 'https://coffeeshop.academy.effective.band/api/v1'));
@@ -9,7 +10,7 @@ Future<List<Product>> fetchProducts({int categoryId = 0, int page = 0, int limit
   if (response.statusCode == 200) {
     final data = response.data!['data'].map((json) => Product.fromJson(json));
     final result = List<Product>.from(data);
-    print('fetching products');
+    debugPrint('fetching products');
     return result;
   } else {
     throw Exception('Failed to load products');
