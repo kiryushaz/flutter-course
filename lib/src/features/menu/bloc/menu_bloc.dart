24413fd19987dcf6cbab1ae3d026141d68380df3
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-04-08 15:54:24 +0600

    разбил функции в отдельные методы

--- lib/src/features/menu/bloc/menu_bloc.dart
+++ lib/src/features/menu/bloc/menu_bloc.dart
@@ -12,62 +12,77 @@ part 'menu_state.dart';
 
 class MenuBloc extends Bloc<MenuEvent, MenuState> {
   MenuBloc() : super(const MenuInitialState()) {
-    on<LoadCategoriesEvent>((event, emit) async {
-      debugPrint("fetching categories");
-      try {
-        final categories = await fetchCategories();
-        emit(MenuLoadingState(
-            categories: categories, items: state.items, cartItems: const []));
-      } catch (e) {
-        emit(MenuFailureState(exception: e));
+    on<LoadCategoriesEvent>(_onLoadCategories);
+    on<LoadItemsEvent>(_onLoadItems);
+    on<AddItemToCartEvent>(_onAddItemToCart);
+    on<RemoveItemFromCartEvent>(_onRemoveItemFromCart);
+    on<ClearCartEvent>(_onClearCart);
+    on<CreateNewOrderEvent>(_onCreateNewOrder);
+  }
+
+  Future<void> _onLoadCategories(
+      LoadCategoriesEvent event, Emitter<MenuState> emit) async {
+    debugPrint("fetching categories");
+    try {
+      final categories = await fetchCategories();
+      emit(MenuLoadingState(
+          categories: categories, items: state.items, cartItems: const []));
+    } catch (e) {
+      emit(MenuFailureState(exception: e));
+    }
+  }
+
+  Future<void> _onLoadItems(
+      LoadItemsEvent event, Emitter<MenuState> emit) async {
+    try {
+      List<Product> items = [];
+      for (var category in state.categories!) {
+        debugPrint("fetching product with category id = ${category.id}");
+        final result = await fetchProducts(categoryId: category.id, limit: 10);
+        items.addAll(result);
       }
-    });
-    on<LoadItemsEvent>((event, emit) async {
-      try {
-        List<Product> items = [];
-        for (var category in state.categories!) {
-          debugPrint("fetching product with category id = ${category.id}");
-          final result =
-              await fetchProducts(categoryId: category.id, limit: 10);
-          items.addAll(result);
-        }
-        emit(MenuSuccessState(
-            categories: state.categories,
-            items: items,
-            cartItems: state.cartItems));
-      } catch (e) {
-        emit(MenuFailureState(exception: e));
-      }
-    });
-    on<AddItemToCartEvent>((event, emit) async {
-      List<Product> cart = List<Product>.from(state.cartItems!);
-      cart.add(event.item);
-      emit(MenuSuccessState(
-          categories: state.categories, items: state.items, cartItems: cart));
-      debugPrint("Added item to cart");
-    });
-    on<RemoveItemFromCartEvent>((event, emit) async {
-      List<Product> cart = List<Product>.from(state.cartItems!);
-      cart.remove(event.item);
-      emit(MenuSuccessState(
-          categories: state.categories, items: state.items, cartItems: cart));
-      debugPrint("Remove item from cart");
-    });
-    on<ClearCartEvent>((event, emit) async {
       emit(MenuSuccessState(
           categories: state.categories,
-          items: state.items,
-          cartItems: const []));
-      debugPrint("Cart cleared");
-    });
-    on<CreateNewOrderEvent>((event, emit) async {
-      try {
-        final result = await createOrder(event.orderJson);
-        debugPrint('$result');
-      } on Exception catch (e) {
-        debugPrint("Exception: $e");
-      }
-      debugPrint("Create new order");
-    });
+          items: items,
+          cartItems: state.cartItems));
+    } catch (e) {
+      emit(MenuFailureState(exception: e));
+    }
+  }
+
+  Future<void> _onAddItemToCart(
+      AddItemToCartEvent event, Emitter<MenuState> emit) async {
+    List<Product> cart = List<Product>.from(state.cartItems!);
+    cart.add(event.item);
+    emit(MenuSuccessState(
+        categories: state.categories, items: state.items, cartItems: cart));
+    debugPrint("Added item to cart");
+  }
+
+  Future<void> _onRemoveItemFromCart(
+      RemoveItemFromCartEvent event, Emitter<MenuState> emit) async {
+    List<Product> cart = List<Product>.from(state.cartItems!);
+    cart.remove(event.item);
+    emit(MenuSuccessState(
+        categories: state.categories, items: state.items, cartItems: cart));
+    debugPrint("Remove item from cart");
+  }
+
+  Future<void> _onClearCart(
+      ClearCartEvent event, Emitter<MenuState> emit) async {
+    emit(MenuSuccessState(
+        categories: state.categories, items: state.items, cartItems: const []));
+    debugPrint("Cart cleared");
+  }
+
+  Future<void> _onCreateNewOrder(
+      CreateNewOrderEvent event, Emitter<MenuState> emit) async {
+    try {
+      final result = await createOrder(event.orderJson);
+      debugPrint('$result');
+    } on Exception catch (e) {
+      debugPrint("Exception: $e");
+    }
+    debugPrint("Create new order");
   }
 }
