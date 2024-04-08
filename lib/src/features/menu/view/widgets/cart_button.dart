bd1419c8755f57c96334157cec72a67cf5af5082
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-04-08 18:05:24 +0600

    fetching bloc via context.watch

--- lib/src/features/menu/view/widgets/cart_button.dart
+++ lib/src/features/menu/view/widgets/cart_button.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_course/src/features/menu/bloc/menu_bloc.dart';
 
 class CartButton extends StatelessWidget {
-  final MenuBloc bloc;
-
-  const CartButton({
-    super.key,
-    required this.bloc,
-  });
+  const CartButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final MenuBloc bloc = context.watch<MenuBloc>();
     final double totalCost = bloc.state.cartItems!.isEmpty
         ? 0.0
         : bloc.state.cartItems!
