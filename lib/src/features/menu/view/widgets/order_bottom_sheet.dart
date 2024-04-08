efb9ad8afec2e9c146ad17e84d56d1d033bb9d5c
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-04-08 18:04:34 +0600

    remove bloc from class, fetching bloc via context.read

--- lib/src/features/menu/view/widgets/order_bottom_sheet.dart
+++ lib/src/features/menu/view/widgets/order_bottom_sheet.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_course/src/features/menu/bloc/menu_bloc.dart';
 import 'package:flutter_course/src/features/menu/model/product.dart';
 
 class OrderBottomSheet extends StatefulWidget {
-  final List<Product> cart;
-  final MenuBloc bloc;
-
-  const OrderBottomSheet({super.key, required this.cart, required this.bloc});
+  const OrderBottomSheet({super.key});
 
   @override
   _OrderBottomSheetState createState() => _OrderBottomSheetState();
@@ -32,7 +30,7 @@ class _OrderBottomSheetState extends State<OrderBottomSheet> {
                             fontSize: 24, fontWeight: FontWeight.bold)),
                     IconButton(
                         onPressed: () {
-                          widget.bloc.add(const ClearCartEvent());
+                          context.read<MenuBloc>().add(const ClearCartEvent());
                           Navigator.of(context).pop();
                         },
                         icon: const Icon(Icons.delete))
@@ -47,30 +45,30 @@ class _OrderBottomSheetState extends State<OrderBottomSheet> {
                   return ListTile(
                       contentPadding: const EdgeInsets.all(10.0),
                       leading: Image.network(
-                          widget.bloc.state.cartItems![index].imageUrl,
+                          context.read<MenuBloc>().state.cartItems![index].imageUrl,
                           width: 55.0,
                           height: 55.0,
                           fit: BoxFit.contain),
                       title: Padding(
                         padding: const EdgeInsets.only(left: 10.0),
-                        child: Text(widget.bloc.state.cartItems![index].name,
+                        child: Text(context.read<MenuBloc>().state.cartItems![index].name,
                             style: Theme.of(context).textTheme.titleMedium),
                       ),
                       subtitle: Padding(
                         padding: const EdgeInsets.only(left: 10.0),
                         child: Text(
-                            widget.bloc.state.cartItems![index].description,
+                            context.read<MenuBloc>().state.cartItems![index].description,
                             style: Theme.of(context).textTheme.bodySmall),
                       ),
                       trailing: Text(
-                          widget.bloc.state.cartItems![index].prices[0]
+                          context.read<MenuBloc>().state.cartItems![index].prices[0]
                               .toString(),
                           style: const TextStyle(
                               fontSize: 16, fontWeight: FontWeight.bold)));
                 },
                 separatorBuilder: (context, index) =>
                     const SizedBox(height: 10),
-                itemCount: widget.bloc.state.cartItems!.length),
+                itemCount: context.read<MenuBloc>().state.cartItems!.length),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 10.0),
               child: ElevatedButton(
@@ -78,14 +76,11 @@ class _OrderBottomSheetState extends State<OrderBottomSheet> {
                       shape: const RoundedRectangleBorder(
                           borderRadius: BorderRadius.all(Radius.circular(16)))),
                   onPressed: () {
-                    final orderJson = createOrder(widget.bloc.state.cartItems!);
+                    final orderJson = createOrder(context.read<MenuBloc>().state.cartItems!);
                     debugPrint('$orderJson');
-                    widget.bloc.add(CreateNewOrderEvent(orderJson));
+                    context.read<MenuBloc>().add(CreateNewOrderEvent(orderJson));
                     Navigator.of(context).pop();
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text(AppLocalizations.of(context)!.snackBarSuccessMsg),
-                        duration: const Duration(seconds: 2)));
-                    widget.bloc.add(const ClearCartEvent());
+                    context.read<MenuBloc>().add(const ClearCartEvent());
                   },
                   child: SizedBox(
                       width: double.infinity,
