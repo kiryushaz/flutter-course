03547bbb977816864572e702b6c934f19c75d21c
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-04-19 23:45:20 +0600

    getting user's device location

--- lib/src/features/map/map_screen.dart
+++ lib/src/features/map/map_screen.dart
@@ -1,10 +1,65 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_course/src/features/map/widgets/locations_list.dart';
 import 'package:flutter_course/src/theme/app_colors.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void _showSnackBar(String msg) {
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(msg), duration: const Duration(seconds: 2)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +67,19 @@ class MapScreen extends StatelessWidget {
       body: Stack(
         children: [
           YandexMap(
-            onMapCreated: (controller) {
-              
+            onMapCreated: (controller) async {
+              try {
+                final position = await _determinePosition();
+                controller
+                    .moveCamera(CameraUpdate.newCameraPosition(CameraPosition(
+                        target: Point(
+                  latitude: position.latitude,
+                  longitude: position.longitude,
+                ))));
+              } on Exception catch (e) {
+                _showSnackBar(e.toString());
+                log(e.toString());
+              }
             },
           ),
           Padding(
@@ -23,14 +89,13 @@ class MapScreen extends StatelessWidget {
               children: [
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.all(10.0),
-                    minimumSize: Size.zero,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    backgroundColor: CoffeeAppColors.cardBackground,
-                    foregroundColor: CoffeeAppColors.primaryTextColor
-                  ),
+                      padding: const EdgeInsets.all(10.0),
+                      minimumSize: Size.zero,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      backgroundColor: CoffeeAppColors.cardBackground,
+                      foregroundColor: CoffeeAppColors.primaryTextColor),
                   onPressed: () {
                     Navigator.of(context).pop();
                   },
