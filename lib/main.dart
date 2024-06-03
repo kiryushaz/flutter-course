12beba155fcc731efc3e5e2c51b090bb8bd5f540
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-06-04 02:31:30 +0600

    added push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,44 @@
 import 'dart:async';
 import 'dart:developer';
-
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_course/src/app.dart';
+import 'package:flutter_course/firebase_options.dart';
+import 'package:flutter_course/src/common/notification.dart';
 import 'package:flutter_course/src/bloc_observer.dart';
-import 'package:yandex_mapkit/yandex_mapkit.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_course/src/app.dart';
 
-import 'package:bloc/bloc.dart';
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {}
 
-void main() {
-  AndroidYandexMap.useAndroidViewSurface = false;
-  Bloc.observer = const MyBlocObserver();
+void main() async {
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    Bloc.observer = const MyBlocObserver();
 
-  runZonedGuarded(() => runApp(const CoffeeShopApp()), (error, stack) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: true,
+      sound: true,
+    );
+
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    LocalNotificationService.initialize();
+
+    FirebaseMessaging.onMessage
+        .listen((message) => LocalNotificationService.display(message));
+
+    runApp(const CoffeeShopApp());
+  }, (error, stack) {
     log(error.toString(), name: 'App Error', stackTrace: stack);
   });
-
-  WidgetsFlutterBinding.ensureInitialized();
 }
